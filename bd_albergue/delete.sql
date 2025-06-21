ed6137e0b161b39726442b49352f5ef1e1d02afc
fbrtz <113561869+fbrtz@users.noreply.github.com>
2025-06-20 21:52:18 -0300

    Atualização delete

--- bd_albergue/delete.sql
+++ bd_albergue/delete.sql
@@ -1,11 +1,10 @@
+use albergue_almeida;
+
 -- Deletando um pagamento
 DELETE FROM Pagamento WHERE id_pagamento = 2;
 
 -- Deletando uma reserva vaga
 DELETE FROM Reserva_Vaga WHERE id_reserva = 2 AND id_vaga = 5;
-
--- Deletando uma peculiaridade
-DELETE FROM Peculiaridades WHERE nome = 'Ao Lado da Porta';
 
 -- Deletando uma vaga
 DELETE FROM Vaga WHERE id_vaga = 8;
